0fcc6d8393af798b8be7bd1a653d8331d567247e
Kolimaro <saithikus@gmail.com>
2020-04-17 15:45:24 +0800

    2.2.1

---
+++ src/main/java/hiber/dao/CarDaoImpl.java
@@ -0,0 +1,36 @@
+package hiber.dao;
+
+import hiber.model.Car;
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class CarDaoImpl implements CarDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public void add(Car car) {
+        sessionFactory.getCurrentSession().save(car);
+    }
+
+    @Override
+    public List<Car> listCars() {
+        TypedQuery<Car> query = sessionFactory.getCurrentSession().createQuery("from Car", Car.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public User getUserByCar(String name, int series) {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("select c.user from Car c where c.name = :car_name and c.series = :car_series", User.class);
+        query.setParameter("car_name", name);
+        query.setParameter("car_series", series);
+        return query.getSingleResult();
+    }
+}
